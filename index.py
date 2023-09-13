beb0564f52ba47aba14efa0f4e40358cc27bbfd4
babiryesandra <143062934+babiryesandra@users.noreply.github.com>
2023-09-13 07:49:43 -0700

    Add files via upload
    computer programming in python

---
+++ index.py
@@ -0,0 +1,40 @@
+# even numbers
+even_numbers =([2,4,6])
+sum_of_even_numbers =([2+4+6])
+print(sum_of_even_numbers)
+
+# number of students
+total_sum =(1, 2, 3, 4, 5)
+average= total_sum/5
+print(average)
+
+
+#-5+8*6
+e=-5
+s=(8*6)
+t=(e+s)
+print(t)
+
+ 
+#55+9*%9
+r=(55+9)
+u=9
+new_value=(r%u)
+print (new_value)
+
+
+
+20+-3*5/8
+n=-3*(5/8)
+print(20+n)
+
+#5+ 15/3*5-8%3
+y=(5+(15/3)*2)-8
+x=3
+print(y%x)
+
+
+
+
+
+
